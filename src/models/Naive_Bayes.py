eba4826ef81228c0ab44aaa8bbea427684763d09
jodi106 <jonas@dierenbach.ch>
2022-05-17 18:42:37 +0200

    Implemented Count_Vectorizer, impactful words

--- src/models/Naive_Bayes.py
+++ src/models/Naive_Bayes.py
@@ -9,9 +9,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
-def train_mvb_bayes(df_train: pd.DataFrame, tfidf: TfidfVectorizer):
+def train_mvb_bayes(df_train: pd.DataFrame, vect: TfidfVectorizer):
 
-    Xt_train = tfidf.transform(df_train['preprocessed'])
+    Xt_train = vect.transform(df_train['preprocessed'])
     y_train = df_train['label']
 
     # Multi-variate Bernoulli Naive Bayes
@@ -20,9 +20,9 @@ def train_mvb_bayes(df_train: pd.DataFrame, tfidf: TfidfVectorizer):
 
     return bnb
 
-def train_mn_bayes(df_train: pd.DataFrame, tfidf: TfidfVectorizer):
+def train_mn_bayes(df_train: pd.DataFrame, vect: TfidfVectorizer):
 
-    Xt_train = tfidf.transform(df_train['preprocessed'])
+    Xt_train = vect.transform(df_train['preprocessed'])
     y_train = df_train['label']
 
     # Multinominal Naive Bayes
@@ -32,9 +32,9 @@ def train_mn_bayes(df_train: pd.DataFrame, tfidf: TfidfVectorizer):
     return mnb
 
 
-def test_model(model, df_test: pd.DataFrame, tfidf: TfidfVectorizer):
+def test_model(model, df_test: pd.DataFrame, vect: TfidfVectorizer):
     
-    Xt_test = tfidf.transform(df_test['preprocessed'])
+    Xt_test = vect.transform(df_test['preprocessed'])
     y_test = df_test['label']
     y_pred = model.predict(Xt_test)
 
@@ -47,3 +47,18 @@ def test_model(model, df_test: pd.DataFrame, tfidf: TfidfVectorizer):
 
     return predictions
 
+def get_impact_words(df_train: pd.DataFrame, vect: TfidfVectorizer, model = nb.MultinomialNB()):
+    
+    Xt_train = vect.transform(df_train['preprocessed'])
+    y_train = df_train['label']
+  
+    model.fit(Xt_train, y_train);
+    
+    model.feature_log_prob_
+    model.coef_
+
+    feature_names = vect.get_feature_names_out()
+    for i, class_label in enumerate(['no_hate', 'hate']):
+        top10 = np.argsort(model.feature_log_prob_[i])[-10:]
+        print("%s: %s" % (class_label,
+            " ".join(feature_names[j] for j in top10)))
